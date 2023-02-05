707c9453925b0ac647fee5333bc3f78ad5bb5820
Tatevik <tshavarshevna@gmail.com>
2023-02-06 00:27:31 +0300

    cube

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 1;
+
+while (sum <= num)
+{
+    int cube = sum * sum * sum;
+            if (cube % 2 == 0)
+                
+            {
+                Console.WriteLine($"число " + sum);
+                Console.WriteLine($"куб  " + cube);
+            }
+   
+   sum++; 
+} 
+
+    
+
+   
+
+
